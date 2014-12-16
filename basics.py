58606c82cb00441c8704c78c83e49b58d8218d01
gregor <gregor.olenik@web.de>
2014-12-16 14:02:54 +0100

    updated slides

--- basics.py
+++ basics.py
@@ -10,12 +10,12 @@
 # Why Python
 # Simple to learn (hard to master?)
 # Clean Syntax, no unneeded cruft
-# Batteries included, numereous modules on pypi
+# Batteries included, numerous modules on pypi
 # Many Python API's e.g. Paraview
 ##
 
 # Active community
-# https://www.youtube.com/watch?v=OSGv2VnC0go # Hettinger - Beatiful Code
+# https://www.youtube.com/watch?v=OSGv2VnC0go # Hettinger - Beautiful Code
 # https://www.youtube.com/watch?v=RrPZza_vZ3w # Beazly Beginner Tutorial
 # https://www.youtube.com/watch?v=xe_ATRmw0KM # Perez - Ipython
 # https://www.youtube.com/watch?v=w26x-z-BdWQ # Kinney - Pandas
@@ -31,19 +31,25 @@
 # Install it:
 # sudo apt-get install ipython ipython-notebook python-pip
 ##
-
 # Data Structures - Lists
 empty_list  = []
 
-example_list = [1,2,3,"a","b","c"] #extremly important
+example_list = [1,2,3,"a","b","c"] #extremely important
 
-example_persons = ["Alice", 1.60, "Bob", 1.80] # but dont do this
+example_persons = ["Alice", 1.60, "Bob", 1.80] # but don't do this
 
 example_persons_objects =  [
         ("Alice", 1.60), # or use custom class 
         ("Bob", 1.80),   # e.g Person("Alice",1.60) ..
     ] 
 ##
+# Data Structures - Tuples
+# Tuples are just fixed size lists
+
+example_tuple = (1,2)
+a,b = (1,2) # Tuple unpacking
+example_tuple2 = 3,4
+## 
 
 # Data Structures - Dictionaries
 example_dict = {'a':'abc', 1:123}
@@ -87,7 +93,16 @@ def func(a,b):
 def func_2(a,b):
     """ a function """
     return 2*a,2*b
-
-
-
 ## 
+
+# Best Practise Functions
+
+# Write functions to build logical units
+
+# Defining a functions gives a comment for 
+# free by its name
+
+# Seperate responsibilites
+
+# Allow unit tests
+##
